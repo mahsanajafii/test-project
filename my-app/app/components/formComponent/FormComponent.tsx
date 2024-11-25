ce238dbdbdae7b3454bd7ea1dd577a344042fa03
mahsanajafi <mahsa.najafi2006@gmail.com>
2024-11-25 14:52:36 +0330

    rename second page

--- my-app/app/components/formComponent/FormComponent.tsx
+++ my-app/app/components/formComponent/FormComponent.tsx
@@ -10,21 +10,21 @@ import Link from "next/link";
   return (
     <form  className="flex justify-between gap-4">  
     
-      <Link href="/secendPage" passHref>  
+      <Link href="/secondPage" passHref>  
         <button  onClick={() => handleClick("yellow")} className="bg-yellow-300 text-lg text-black w-20 rounded-xl h-10 font-bold hover:text-white   shadow-yellow-50 shadow-inner ">  
           Yellow  
         </button>  
       </Link>  
       
     
-      <Link href="/secendPage" passHref>  
+      <Link href="/secondPage" passHref>  
         <button  onClick={() => handleClick("blue")}  className="bg-blue-300 text-lg text-black w-20 rounded-xl h-10 font-bold hover:text-white   shadow-blue-50 shadow-inner">  
           Blue  
         </button>  
       </Link>  
       
     
-      <Link href="/secendPage" passHref>  
+      <Link href="/secondPage" passHref>  
         <button  onClick={() => handleClick("red")}  className="bg-red-300 text-lg text-black w-20 rounded-xl h-10 font-bold hover:text-white   shadow-red-50 shadow-inner ">  
           Red  
         </button>  
